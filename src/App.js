f911b08dbd1a7ca7997b2c438f9cff01c7065a91
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2021-10-22 14:28:04 -0300

    Teste com uma tabela csv

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { useState, useEffect } from "react";
-import  Navbar    from "./components/NavBar";
+import Navbar from "./components/NavBar";
 import "./App.css";
 import { EntryForm } from "./components/Form/EntryForm";
 
+const ParseCSV = (text) => {
+  const result = {
+    header: [],
+    data: [],
+  };
+  const [header, ...content] = text.split("\n");
+
+  result.header = header.split(",");
+
+  content.forEach((item) => {
+    result.data.push(item.split(","));
+  });
+  console.log(result);
+};
+
 export default function App() {
+  useEffect(() => {
+    fetch('./teste.csv')
+      .then((r) => r.text())
+      .then((text) => {
+        ParseCSV(text);
+      });
+  }, []);
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <EntryForm />
+      <div className="App"></div>
     </div>
   );
 }
